bb80ef40356be4384b0ddf0e4510865d4d33c654
Scott Vitale <scott@spigotlabs.com>
2017-05-19 15:49:03 -0700

    Use a newer method for specifying the vendored packages directory.

--- appengine_config.py
+++ appengine_config.py
@@ -1,9 +1,5 @@
 """
 `appengine_config` gets loaded when starting a new application instance.
 """
-import site
-import os.path
-
-# add `lib` subdirectory as a site packages directory, so our `main` module can load
-# third-party libraries.
-site.addsitedir(os.path.join(os.path.dirname(__file__), 'lib'))
+from google.appengine.ext import vendor
+vendor.add('lib')
