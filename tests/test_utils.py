df7f04f1f56c21979430c16db036696f9d14065e
Scott Vitale <scott@spigotlabs.com>
2018-01-19 10:07:44 -0700

    Fixes #11, store the last good results from the Meetup API in a module variable.  The only error I saw in practice was a failed JSON decode on a 200 response.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,7 +8,7 @@
 
 import pytest
 
-from application.utils import TrelloClient
+from application.utils import TrelloClient, pluck
 
 
 class MockTrelloBoard():
@@ -45,3 +45,21 @@ class TestUtils:
         mocker.patch('application.utils.Trello', new=MockTrelloClient)
         client, lst = TrelloClient()
         assert lst['id'] == 1
+
+    def test_pluck(self, *args, **kwargs):
+        good_iterable = [
+            {'name': 'PyColorado'},
+            {'name': 'DenverPython'},
+            {'name': 'BoulderPython'}
+        ]
+        assert pluck(good_iterable, lambda x: x['name'] == 'BoulderPython') == good_iterable[-1]
+
+        empty_iterable = []
+        assert pluck(empty_iterable, lambda x: True) is None
+
+        bad_iterable1 = None
+        assert pluck(bad_iterable1, lambda x: True) is None
+
+        bad_iterable2 = 5
+        assert pluck(bad_iterable2, lambda x: True) is None
+
